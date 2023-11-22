33bf39ab85ef092d04d85e22566370d26f1d89bb
Trevor WM <Trevormoore.2016@gmail.com>
2023-11-21 19:32:55 -0800

    setting up homepage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,9 +1,34 @@
+import { Link, NavLink } from "react-router-dom";
 import "./css/HomePage.css";
 
 const HomePage = () => {
     return (
         <div>
             Hello World!
+            <div className="headShotContainer">
+                <img className="headShot"
+                src={'https://res.cloudinary.com/dfrj03hsi/image/upload/v1693878067/Huitzilin/166FE277-043C-40C3-820C-1E8CC74A8D26rg7_0881_li3l0z.jpg'} alt='preview' />
+            </div>
+            <div className="welcomeBody">
+                Welcome to Huitzilin Transformational Life Coaching!
+                My name is Angela and I am a transformation coach.
+                For almost two decades I have coached people through
+                acting, singing, writing, and simplifying cluttered
+                offices, homes, and minds. The common result in the
+                coaching I’ve provided has been transformation.
+                I’ve been blessed to witness my clients gain the capacity,
+                confidence, and courage to live life on their own terms.
+                Read on to see if working with me is right for you,
+                then you can schedule a call
+                {" "}
+                {/* <Link path="https://calendly.com/colibritlc/30min">here</Link> */}
+                <a
+                href="https://calendly.com/colibritlc/30min"
+                target="_blank"
+                rel="noReferrer"
+                >here</a>
+                .
+            </div>
         </div>
     )
 }
